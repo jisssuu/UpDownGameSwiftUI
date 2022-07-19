8ce66c63e71f4af9d4d7e5c6b021fd6a2fcaa9fb
HOjin <jeonghojin03@gmail.com>
2022-07-19 23:24:13 +0900

    Initial Commit

---
+++ UpDownGameSwiftUI/UpDownGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpDownGameSwiftUIApp.swift
+//  UpDownGameSwiftUI
+//
+//  Created by 홍길동 on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct UpDownGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
