2da49e79b4aad73f4f3adcd956dc78183c7e2465
Jisssuu <wkdwltn1015@naver.com>
2022-07-21 10:51:13 +0900

    UpDownGame Clear!

--- UpDownGameSwiftUI/ContentView.swift
+++ UpDownGameSwiftUI/ContentView.swift
@@ -13,15 +13,35 @@ struct ContentView: View {
     @State var sliderMinValue : Float = 0
     @State var sliderMaxValue : Float = 30
     @State var gameStatus = false
+    @State var gameSuccessed = false
     @State var gameFailed = false
+    @State var information = false
     @State var count = 1
-    let guessNum = Int.random(in: 1...30)
+    @State var guessNum = Int.random(in: 1...30)
     var body: some View {
         VStack {
-            
-            Text("UpDownGame !!")
-                .padding()
-                .font(.title)
+            HStack {
+                Spacer()
+                Button {
+                    information = true
+                } label: {
+                    Image(systemName: "info.circle")
+                        .padding()
+                }.alert( isPresented: $information){
+                    Alert(title: Text("Introduce developer"),message: Text("Developer is jisssuu"),dismissButton: .default(Text("OK")))
+                }
+                Spacer()
+                Text("UpDownGame !!")
+                    .padding()
+                    .font(.title)
+                Spacer()
+                Button {
+                    start()
+                } label: {
+                    Image(systemName: "restart")
+                }
+                Spacer()
+            }
             Text("\(Int(self.sliderValue))")
             HStack {
                 Text("\(Int(sliderMinValue))")
@@ -35,7 +55,7 @@ struct ContentView: View {
                 sliderNowValue = Int(sliderValue)
                 if sliderNowValue == guessNum {
                     gameStatus = true
-                    
+                    gameSuccessed = true
                 }
                 else {
                     if sliderNowValue < guessNum {
@@ -47,6 +67,7 @@ struct ContentView: View {
                         count += 1
                     }
                     if(count == 5){
+                        gameStatus = true
                         gameFailed = true
                     }
                 
@@ -55,16 +76,25 @@ struct ContentView: View {
                 Text("button")
                     .padding()
             }.alert(isPresented: $gameStatus) {
-                Alert(title: Text("UpDownGame"), message: Text("HIT!"), dismissButton: .default(Text("OK")))
+                if gameSuccessed == true {
+                   return Alert(title: Text("UpDownGame"), message: Text("HIT!"), dismissButton: .default(Text("OK")) )
+                }
+                else {
+                   return Alert(title: Text("UpDownGame"), message: Text("You Lose!"), dismissButton: .default(Text("OK")))
+                
+                }
             }
-            .alert(isPresented: $gameFailed) {
-                Alert(title: Text("UpDownGame"), message: Text("You Lose!"), dismissButton: .default(Text("OK")))
-            }
-            Text("Your guess number is \(sliderNowValue)")
-            Text("Your min number is \(Int(sliderMinValue))")
-            Text("Your max number is \(Int(sliderMaxValue))")
             Text("\(count) / 5 ")
         }
+    }
+    func start(){
+        sliderNowValue = 0
+        sliderMaxValue = 30
+        sliderMinValue = 0
+        sliderValue = 15
+        let reGuessNumber = Int.random(in: 1...30)
+        guessNum = reGuessNumber
+        count = 1
     }
 }
 
